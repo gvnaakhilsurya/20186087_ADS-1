55a4a8590a9e76317ed1acb2afacb9a3edf6b4f8
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-26 23:07:13 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- Assignments/m3/Sorted_Array/Solution.java
+++ Assignments/m3/Sorted_Array/Solution.java
@@ -1,39 +1,60 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+/**.
+ * Class for solution.
+ */
 public class Solution{
+	/**.
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      b     { parameter_description }
+	 */
 	public void mergeArrays(int[] a,int[] b){
+		/**.
+		 * { var_description }
+		 */
 		int i = 0;
 		int j = 0;
 		int k = 0;
 		int[] c = new int[a.length+b.length];
-		while(i<a.length && j<b.length){
+		while (i < a.length && j < b.length){
 			if (a[i]<b[j]) {
 				c[k++]=a[i];
 				i++;
-			}else{
+			} else {
 				c[k++] = b[j];
 				j++;
 			}
 		}
-			if(i == a.length){
-				while(j<b.length){
+			if (i == a.length){
+				while (j < b.length){
 					c[k++]= b[j];
 					j++;
 				}
 			}
 			else {
-					while(i<a.length) {
+					while(i < a.length) {
 					c[k++] = a[i];
 					i++;
 
 				}
 			}
 		int x;
-		for (x=0;x<c.length-1;x++) {
+		for (x = 0; x < c.length-1; x++) {
 			System.out.print(c[x]+",");
 		}
 		System.out.println(c[x]);
 	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
+		/**.
+		 * { var_description }
+		 */
 		Scanner sc = new Scanner (System.in);
 		//reading the first line input
 		int m = Integer.parseInt(sc.nextLine());
@@ -44,7 +65,7 @@ public class Solution{
 		String[] elements;
 		if(m!= 0){
 		 elements = sc.nextLine().split(",");
-		for (int i = 0;i< elements.length;i++) {
+		for (int i = 0; i < elements.length; i++) {
 			a[i] = Integer.parseInt(elements[i]);
 		}
 	}else{
