e4679cc852ccd1754dc834266a71bd185c08a76b
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-27 15:54:30 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- Assignments/m3/Sorted_Array/Solution.java
+++ Assignments/m3/Sorted_Array/Solution.java
@@ -1,84 +1,88 @@
 import java.util.Scanner;
-import java.util.Arrays;
-/**.
- * Class for solution.
+/**
+ *Solution class.
+ **/
+
+public final class  Solution {
+
+/**
+ *Solution class.
  */
-public class Solution{
-	/**.
-	 * { function_description }
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 */
-	public void mergeArrays(final int[] a,final int[] b){
-		/**.
-		 * { var_description }
-		 */
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		int[] c = new int[a.length+b.length];
-		while (i < a.length && j < b.length){
-			if (a[i]<b[j]) {
-				c[k++]=a[i];
-				i++;
-			} else {
-				c[k++] = b[j];
-				j++;
-			}
-		}
-			if (i == a.length){
-				while (j < b.length){
-					c[k++]= b[j];
-					j++;
-				}
-			}
-			else {
-					while(i < a.length) {
-					c[k++] = a[i];
-					i++;
+private Solution() {
 
-				}
-			}
-		int x;
-		for (x = 0; x < c.length-1; x++) {
-			System.out.print(c[x]+",");
-		}
-		System.out.println(c[x]);
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		/**.
-		 * { var_description }
-		 */
-		Scanner sc = new Scanner (System.in);
-		//reading the first line input
-		int m = Integer.parseInt(sc.nextLine());
-		// reading second line input
-		int n = Integer.parseInt(sc.nextLine());
-		int[] a = new int[m];
-		int[] b = new int[n];
-		String [] elements;
-		if (m!= 0){
-		 elements = sc.nextLine().split(",");
-		for (int i = 0; i < elements.length; i++) {
-			a[i] = Integer.parseInt(elements[i]);
-		}
-	}else{
-		sc.nextLine();
-	}
-	    if (n!= 0) {
-		elements = sc.nextLine().split(",");
-		for (int i = 0; i < elements.length; i++) {
-			b[i] = Integer.parseInt(elements[i]);
-		}
-	}
-		new Solution().mergeArrays(a, b);
+}
 
+/**.
+     * { Merge Arrays }
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     */
+    public void mergeArrays(final int[] a, final int[] b) {
 
-	}
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] c = new int[a.length + b.length];
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i];
+                i++;
+            } else {
+                c[k++] = b[j];
+                j++;
+            }
+        }
+        if (i == a.length) {
+            while (j < b.length) {
+                c[k++] = b[j];
+                j++;
+            }
+        } else {
+            while (i < a.length) {
+                c[k++] = a[i];
+                i++;
+
+            }
+        }
+        int x;
+        for (x = 0; x < c.length - 1; x++) {
+            System.out.print(c[x] + ",");
+        }
+        System.out.println(c[x]);
+    }
+
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param args arguments
+ */
+    public static void main(final String[] args) {
+
+/**
+ * @brief [brief description]
+ * @details [long description]
+ */
+        Scanner sc = new Scanner(System.in);
+        int m = Integer.parseInt(sc.nextLine());
+        int n = Integer.parseInt(sc.nextLine());
+        int[] a = new int[m];
+        int[] b = new int[n];
+        String[] elements;
+        if (m != 0) {
+            elements = sc.nextLine().split(",");
+            for (int i = 0; i < elements.length; i++) {
+                a[i] = Integer.parseInt(elements[i]);
+            }
+        } else {
+            sc.nextLine();
+        }
+        if (n != 0) {
+            elements = sc.nextLine().split(",");
+            for (int i = 0; i < elements.length; i++) {
+                b[i] = Integer.parseInt(elements[i]);
+            }
+        }
+        new Solution().mergeArrays(a, b);
+    }
 }
