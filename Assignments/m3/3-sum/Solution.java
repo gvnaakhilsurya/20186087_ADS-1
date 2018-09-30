13dc47be518dbf5e35e88cda169e9585bbd43fc6
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-09-30 23:57:31 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/m3/3-sum/Solution.java
+++ Assignments/m3/3-sum/Solution.java
@@ -1,62 +1,77 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution{
-	Solution(){
+/**.
+ * Class for solution.
+ */
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * It is the constructor for the Solution class.
+         */
+    }
+    /**.
+     * It is the main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * The main function converts the given input into the 
+         * integer array and intializes the each element used in the binary search.
+         */
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = s.nextInt();
+        }
+        Arrays.sort(arr);
+        boolean ans;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int low = j + 1;
+                int high = size - 1;
+                ans = search(low, high, arr, -(arr[i] + arr[j]));
+                if (ans) {
+                    count++;
+                }
 
-	}
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int inputs = sc.nextInt();
-		int[]numarry = new int[inputs];
-		for (int i = 0 ;i<inputs;i++) {
-			numarry[i] = sc.nextInt();
-		}
-		Arrays.sort(numarry);
-		boolean result;
-		int count = 0;
-		for (int i = 0;i < inputs;i++) {
-			for (int j = i;j < inputs ;j++) {
-				int low = j+1;
-				int high = inputs-1;
-				result = search(low,high,numarry, -(numarry[i] + numarry[j]));
-				if (result) {
-					count++;
-						
-				}	
-			}	
-		}
-		 System.out.println(count);
-    
-	}
-
-
-	public static boolean search(final int low, final int high,
+            }
+        }
+        System.out.println(count);
+    }
+    /**.
+     * Searches for the first match.
+     *
+     * @param      l     lowest element in the integer array 
+     * @param      h     highest element in the integer array
+     * @param      b     integer array
+     * @param      s     Search element 
+     *
+     * @return     boolen value
+     */
+    public static boolean search(final int l, final int h,
         final int[] b, final int s) {
-
-		int midnum = 0;
-		int count = 0;
-		int lownum = low;
-		int highnum = high;
-		int[]numarry = b;
-		int search = s;
-		while(lownum <=highnum){
-			midnum = (lownum + highnum)/2;
-			if (search ==numarry[midnum]) {
-				return true;
-			} else if(numarry[midnum]>search){
-				highnum = midnum -1;	
-			} else{
-				lownum = midnum +1;
-			}
-
-			}
-			return false;
-
-		}
-	}
-
-
-
-
-
+                    int middle = 0;
+                    int count = 0;
+                    int low = l;
+                    int high = h;
+                    int[] arr = b;
+                    int search = s;
+                    while (low <= high) {
+                    middle = (low + high) / 2;
+                    if (search == arr[middle]) {
+                        return true;
+                    } else if (arr[middle] > search) {
+                        high = middle - 1;
+                    } else {
+                        low = middle + 1;
+                    }
+                }
+                return false;
+            }
+        }
