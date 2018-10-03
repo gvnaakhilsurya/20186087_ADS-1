3a6f3df78e114aaf8d6313ad2c0b1d6c6dcd374a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-03 13:24:39 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ Assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -16,22 +16,24 @@ public class Solution{
 		 */
 		Scanner sc = new Scanner(System.in);
 		StringBuffer sb = new StringBuffer();
-		final int THREE = 3;
-		while (sc.hasNext()){
+		final int p = 3;
+		while (sc.hasNext()) {
 			sb.append(sc.nextLine() + "::");
-		}
+	}
 		String []lines = sb.toString().split("::");
 		int i = 0;
 		Team[] teams = new Team[lines.length];
 		for (String line : lines) {
 			String[]tokens = line.split(",");
-			Team team = new Team(tokens[0], Integer.parseInt(tokens[1]),
-				Integer.parseInt(tokens[2]), Integer.parseInt(tokens[THREE]));
+			Team team = new Team(tokens[0],
+			 Integer.parseInt(tokens[1]),
+			  Integer.parseInt(tokens[2]),
+			   Integer.parseInt(tokens[p]));
 			teams[i++] = team;
-		}
+	}
 		InsertionSort.sort(teams);
 		String output  = Arrays.toString(teams);
-		output = output.replace("[","").replace("]","").replace(", ", ",");
+		output = output.replace("[", "").replace("]", "").replace(", ", ",");
 		System.out.println(output);
 	}
 	/**By analaysis the complexity of the above method is O(N^2).**/
