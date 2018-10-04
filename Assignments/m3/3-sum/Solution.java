9f07b9160183a187a9f2c047d7f03be3cfcb912f
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-04 19:10:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignments/m3/3-sum/Solution.java
+++ Assignments/m3/3-sum/Solution.java
@@ -4,16 +4,16 @@ class Solution {
 	/**
 	 * Constructs the object.
 	 */
-Solution() {
+private Solution() {
 
 }
-/**
+/**.
  * { function_description }
  *
  * @param      args  The arguments
  */
 public static void main(String[] args) {
-	/**
+	/**.
 	 * This method which delals with the main
 	 * method of the three sum.
 	 */
@@ -21,15 +21,15 @@ public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 int inputs = sc.nextInt();
 int[]numarry = new int[inputs];
-for (int i = 0 ;i<inputs;i++) {
+for (int i = 0; i < inputs; i++) {
 	numarry[i] = sc.nextInt();
 }
 Arrays.sort(numarry);
 int count = 0;
 for (int i = 0;i < inputs;i++) {
-	for (int j = i+1 ;j < inputs ;j++) {
+	for (int j = i + 1; j < inputs; j++) {
 		int k = Arrays.binarySearch(numarry, -(numarry[i] + numarry[j]));
-		if(k > j) {
+		if (k > j) {
 			count++;
 		}
 	}	
