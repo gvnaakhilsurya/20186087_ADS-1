2a1f06ce3d4a0b244d84e31a74bb7c2b73318e71
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-27 12:35:15 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m24/1/Student.java
+++ Assignments/m24/1/Student.java
@@ -2,88 +2,85 @@
  * Class for student.
  */
 class Student {
-/**.
-	 * rool no
-	 */
-	private int rollno;
-	/**.
-	 * name of student.
-	 */
-	private String name;
-	/**.
-	 * 
-	 */
-	private double totalmarks;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      rollno      The rollno
-	 * @param      name        The name
-	 * @param      totalmarks  The totalmarks
-	 */
-	Student(final int rollno, final String name, final double totalmarks) {
-		this.rollno = rollno;
-		this.name = name;
-		this.totalmarks = totalmarks;
-	}
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      name        The name
-	 * @param      totalmarks  The totalmarks
-	 */
-	Student(final String name, final double totalmarks) {
-		this.name = name;
-		this.totalmarks = totalmarks;
-	}
-	/**.
-	 * Gets the rollno.
-	 *
-	 * @return     The rollno.
-	 */
-	 public int getRollno() {
-	  return rollno;
-	 }
-	 /**.
-	  * Sets the rollno.
-	  *
-	  * @param      rollno  The rollno
-	  */
-	public void setRollno(final int rollno) {
-	  this.rollno = rollno;
-	}
-	/**.
-	 * Gets the name.
-	 *
-	 * @return     The name.
-	 */
-	 public String getName() {
-	  return name;
-	}
-	/**.
-	 * Sets the name.
-	 *
-	 * @param      name  The name
-	 */
-	public void setName(final String name) {
-	  this.name = name;
-	}
-	/**
-	 * Gets the totalmarks.
-	 *
-	 * @return     The totalmarks.
-	 */
-	public Double getTotalmarks() {
-	   return totalmarks;
-	}
-	/**
-	 * Sets the totalmarks.
-	 *
-	 * @param      totalmarks  The totalmarks
-	 */
-	public void setTotalmarks(final Double totalmarks) {
-	   this.totalmarks = totalmarks;
-	}
+     
+    private int rollno;
+    /**.
+     * name of student.
+     */
+    private String name;
+    /**.
+     *
+     */
+    private double totalmarks;
+    /**.
+     * Constructs the object.
+     *
+     * @param      rollno      The rollno
+     * @param      name        The name
+     * @param      totalmarks  The totalmarks
+     */
+    Student(final int rollno, final String name, final double totalmarks) {
+        this.rollno = rollno;
+        this.name = name;
+        this.totalmarks = totalmarks;
+    }
+    /**.
+     * Constructs the object.
+     *
+     * @param      name        The name
+     * @param      totalmarks  The totalmarks
+     */
+    Student(final String name, final double totalmarks) {
+        this.name = name;
+        this.totalmarks = totalmarks;
+    }
+    /**.
+     * Gets the rollno.
+     *
+     * @return     The rollno.
+     */
+     public int getRollno() {
+      return rollno;
+     }
+     /**.
+      * Sets the rollno.
+      *
+      * @param      rollno  The rollno
+      */
+    public void setRollno(final int rollno) {
+      this.rollno = rollno;
+    }
+    /**.
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+     public String getName() {
+      return name;
+    }
+    /**
+     * Sets the name.
+     *
+     * @param      name  The name
+     */
+    public void setName(final String name) {
+      this.name = name;
+    }
+    /**
+     * Gets the totalmarks.
+     *
+     * @return     The totalmarks.
+     */
+    public Double getTotalmarks() {
+       return totalmarks;
+    }
+    /**
+     * Sets the totalmarks.
+     *
+     * @param      totalmarks  The totalmarks
+     */
+    public void setTotalmarks(final Double totalmarks) {
+       this.totalmarks = totalmarks;
+    }
 
 }
-
