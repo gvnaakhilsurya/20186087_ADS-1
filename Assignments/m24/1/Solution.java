ef9bdfa91d60455cf55926c265aecb00d9df8e54
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-27 11:59:12 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m24/1/Solution.java
+++ Assignments/m24/1/Solution.java
@@ -10,28 +10,26 @@ public class Solution {
 	private Solution() {
 
 	}
+	/**
+	 * In this we takes the input from the user input
+	 * and make it as tokens and then sent to the follwing
+	 * classes we used. 
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int stdrecds = Integer.parseInt(sc.nextLine());
 		HashMap<Integer, Student> hmap = new HashMap<Integer, Student>(stdrecds);
-		// System.out.println(stdrecds);
 		for (int i = 0; i < stdrecds; i++) {
 			String line = sc.nextLine();
 			String[]studata = line.split(",");
-			// for (String stud : studata) {
-			// 	System.out.println(stud);
-				
-			// }
 			hmap.put(Integer.parseInt(studata[0]), new Student(studata[1], Double.parseDouble(studata[2])));
 		}
 		int mquires = Integer.parseInt(sc.nextLine());
-		// System.out.println(mquires);
 		for (int i = 0; i < mquires; i++) {
 			String line = sc.nextLine();
 			String[]qurdata = line.split(" ");
-			// for (String stud : qurdata) {
-			// 	System.out.println(stud);
-			// }
 			  Student student = hmap.get(Integer.parseInt(qurdata[1]));
 			if(student == null){
                 System.out.println("Student doesn't exists...");
--- Assignments/m24/1/Student.java
+++ Assignments/m24/1/Student.java
@@ -1,35 +1,77 @@
+/**
+ * Class for student.
+ */
 class Student {
 	private int rollno;
 	private String name;
 	private double totalmarks;
-
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      rollno      The rollno
+	 * @param      name        The name
+	 * @param      totalmarks  The totalmarks
+	 */
 	Student(int rollno, String name, double totalmarks) {
 		this.rollno = rollno;
 		this.name = name;
 		this.totalmarks = totalmarks;
 	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name        The name
+	 * @param      totalmarks  The totalmarks
+	 */
 	Student(String name, double totalmarks) {
 		this.name = name;
 		this.totalmarks = totalmarks;
 	}
+	/**
+	 * Gets the rollno.
+	 *
+	 * @return     The rollno.
+	 */
 	 public int getRollno(){
 	  return rollno;
 	 }
-	 
+	 /**
+	  * Sets the rollno.
+	  *
+	  * @param      rollno  The rollno
+	  */
 	public void setRollno(int rollno){
 	  this.rollno = rollno;
 	}
+	/**
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	 public String getName(){
 	  return name;
 	}
-	 
+	/**
+	 * Sets the name.
+	 *
+	 * @param      name  The name
+	 */
 	public void setName(String name){
 	  this.name = name;
 	}
+	/**
+	 * Gets the totalmarks.
+	 *
+	 * @return     The totalmarks.
+	 */
 	public Double getTotalmarks(){
 	   return totalmarks;
 	}
-	  
+	/**
+	 * Sets the totalmarks.
+	 *
+	 * @param      totalmarks  The totalmarks
+	 */
 	public void setTotalmarks(Double totalmarks){
 	   this.totalmarks = totalmarks;
 	}
