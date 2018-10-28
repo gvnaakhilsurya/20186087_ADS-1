17531f5eaa8b386f79cf711d5ff7a354ac3be010
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-28 19:28:53 +0530

    "ads1_m9_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ Assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
-class Solution {
+/**
+ * { item_description }
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private  Solution() {
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
