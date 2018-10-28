6cefb67895dd654a384360d51d0e247315db165a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-28 18:41:24 +0530

    "ads1_m7_a1: 2/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,54 @@
+/**
+ * @author : gvnaakhilsurya.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		/**
+		 * Empty constructor.
+		 */
+	}
+	/**
+	 * main method to take input and call steque functions.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numOftestcases = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < numOftestcases; i++) {
+			Steque steobj = new Steque();
+			while (sc.hasNext()) {
+				String line = sc.nextLine();
+				if (line.length() == 0) {
+					System.out.println();
+					break;
+				}
+				String[] tokens = line.split(" ");
+				switch (tokens[0]) {
+					case "push":
+					steobj.push(tokens[1]);
+					System.out.println(steobj);
+					break;
+					case "enqueue":
+					steobj.enqueue(tokens[1]);
+					System.out.println(steobj);
+					break;
+					case "pop":
+					steobj.pop();
+					System.out.println(steobj);
+					break;
+					default:
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Assignments/m7/7.1 Steque/Steque.java
@@ -0,0 +1,98 @@
+/**
+ * @author : gvnaakhilsurya.
+ */
+/**
+ * Class for steque.
+ */
+class Steque {
+    /**
+     * Node start.
+     */
+    private Node start = null;
+    /**
+     * Node end.
+     */
+    private Node end = null;
+/**
+ * Class for node.
+ */
+class Node {
+    /**
+     * String data in a node.
+     */
+    private String data;
+    /**
+     * link for the next node.
+     */
+    private Node next;
+    /**
+     * Constructs the object.
+     *
+     * @param      data1  The data
+     */
+    Node(final String data1)  {
+        this.data = data1;
+        this.next = null;
+    }
+}
+/**
+ * add item to the left of steque.
+ * time complexity is constant.
+ *
+ * @param      item  The item
+ */
+public void push(final String item) {
+    Node newNode = new Node(item);
+    if (start == null) {
+        end = newNode;
+    }
+    newNode.next = start;
+    start = newNode;
+}
+/**
+ * add item to the right of steque.
+ * time complexity is constant.
+ * @param      item  The item
+ */
+public void enqueue(final String item) {
+    if (start == null) {
+        Node newNode = new Node(item);
+        start = newNode;
+        end = newNode;
+    } else {
+        Node newNode1 = new Node(item);
+        end.next = newNode1;
+        end = newNode1;
+    }
+}
+/**
+ * remove the top most element.
+ * time complexity is constant.
+ */
+public void pop() {
+    if (start != null) {
+        start = start.next;
+    }
+    if (end != null) {
+        end = end.next;
+    }
+}
+/**
+ * Returns a string representation of the object.
+ *
+ * @return     String representation of the object.
+ */
+public String toString() {
+    String str = "";
+    Node temp = start;
+    if (temp == null) {
+        return "Steque is empty.";
+    }
+    while (temp.next != null) {
+        str += temp.data + "," + " ";
+        temp = temp.next;
+    }
+    str += temp.data;
+    return str;
+}
+}
