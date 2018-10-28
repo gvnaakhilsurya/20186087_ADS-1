82aaccaa1368086d994a2844a2c2dc5fa88c74cb
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-28 10:58:40 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m22/22.1 Ransom Note/Solution.java
+++ Assignments/m22/22.1 Ransom Note/Solution.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 /**
  * Class for solution.
+ * @author : gvnaakhilsurya
  */
 public class Solution {
 
@@ -11,11 +12,15 @@ public class Solution {
 
 	}
 	/**
-	 * { function_description }
-	 *
+	 * It is the min method which is used to
+	 * read the input from the files and the divided into the
+	 * tokens and pass as the arguments as the specified methods.
+	 * By the analysis the time complexity of the below method isO().
 	 * @param      args  The arguments
 	 */
 	public static void main(String[] args) {
+		/*This part is reding input from the input files and divied into
+		tokens.*/
 		Scanner sc = new Scanner(System.in);
 		int nofwrdsinmagz = sc.nextInt();
 		int nofwrdsinrnote = sc.nextInt();
@@ -23,6 +28,8 @@ public class Solution {
 		sc.nextLine();
 		String[]magzwrds = sc.nextLine().split(" ");
 		String[]rnotewrds = sc.nextLine().split(" ");
+		/*This part is checking whether the words in the ransome note is
+		 present or not*/
 		for(int i = 0; i < magzwrds.length; i++) {
 			if (st.contains(magzwrds[i])) {
 				int count = st.get(magzwrds[i]);
@@ -31,7 +38,8 @@ public class Solution {
 				st.put(magzwrds[i], 1);
 			}
 		}
-
+		/*If it is their the frequency is reduced to one step and if it
+		 is no their break the loop and then print No*/
 		boolean flag = true;
 		for (int i = 0;i < rnotewrds.length;i++) {
 			if (st.contains(rnotewrds[i])) {
