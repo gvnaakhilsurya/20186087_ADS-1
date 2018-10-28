181c6e331102f8c3417f1a163d2518581f09710a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-10-28 12:46:16 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m19/19.1 BST/Solution.java
+++ Assignments/m19/19.1 BST/Solution.java
@@ -21,39 +21,39 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        BinarySearchT<Book, Integer> binarysearcht = new BinarySearchT<>();
+        BinarySearchT<Book, Integer> bst = new BinarySearchT<>();
         while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(",");
             switch (tokens[0]) {
             case "put":
                 Book book = new Book(tokens[1], tokens[1 + 1],
                                      Double.parseDouble(tokens[2 + 1]));
-                binarysearcht.put(book, Integer.parseInt(tokens[2 + 2]));
+                bst.put(book, Integer.parseInt(tokens[2 + 2]));
                 break;
             case "get":
                 Book book1 = new Book(tokens[1], tokens[1 + 1],
                                       Double.parseDouble(tokens[2 + 1]));
-                System.out.println(binarysearcht.get(book1));
+                System.out.println(bst.get(book1));
                 break;
             case "min":
-                System.out.println(binarysearcht.min());
+                System.out.println(bst.min());
                 break;
             case "max":
-                System.out.println(binarysearcht.max());
+                System.out.println(bst.max());
                 break;
             case "floor":
                 Book book2 = new Book(tokens[1], tokens[1 + 1],
                                       Double.parseDouble(tokens[2 + 1]));
-                System.out.println(binarysearcht.floor(book2));
+                System.out.println(bst.floor(book2));
                 break;
             case "ceiling":
                 Book book5 = new Book(tokens[1], tokens[1 + 1],
                                       Double.parseDouble(tokens[2 + 1]));
-                System.out.println(binarysearcht.ceiling(book5));
+                System.out.println(bst.ceiling(book5));
                 break;
             case "select" :
                 System.out.println(
-                    binarysearcht.select(Integer.parseInt(tokens[1])));
+                    bst.select(Integer.parseInt(tokens[1])));
                 break;
             default:
                 break;
