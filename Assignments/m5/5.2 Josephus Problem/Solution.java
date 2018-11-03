fccfb8bafe00f0698e5073afa8dca4c0e77a731a
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2018-11-03 18:11:11 +0530

    "ads1_m5_a2: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m5/5.2 Josephus Problem/Solution.java
+++ Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -4,42 +4,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
+/**.
+* Constructs the object.
+ */
+    private Solution() {
 
-	}
-	/**.
-	 * In the main method we can divide the given
-	 * input files into the tokens and the passes
-	 * through the arguments as the specified methods.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String testcases = sc.nextLine();
-		while (sc.hasNext()) {
-			String[] line = sc.nextLine().split(" ");
-			int people = Integer.parseInt(line[0]);
-			int gap = Integer.parseInt(line[1]);
-			Deque<Integer> q = new Deque<>();
-			createCircle(q, people);
-			q.josephus(people, gap);
-		}
-	}
+    }
+    /**.
+     * In the main method we can divide the given
+     * input files into the tokens and the passes
+     * through the arguments as the specified methods.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String testcases = sc.nextLine();
+        while (sc.hasNext()) {
+            String[] line = sc.nextLine().split(" ");
+            int people = Integer.parseInt(line[0]);
+            int gap = Integer.parseInt(line[1]);
+            Deque<Integer> q = new Deque<>();
+            createCircle(q, people);
+            q.josephus(people, gap);
+        }
+    }
 
-	/**
-	 * Creates a circle.
-	 *
-	 * @param      q       The quarter
-	 * @param      people  The people
-	 */
-	public static void createCircle(final Deque<Integer> q,
-	 final int people) {
-		for (int i = 0; i < people; i++) {
-			q.push(i);
-		}
-	}
+    /**
+     * Creates a circle.
+     *
+     * @param      q       The quarter
+     * @param      people  The people
+     */
+    public static void createCircle(final Deque<Integer> q,
+     final int people) {
+        for (int i = 0; i < people; i++) {
+            q.push(i);
+        }
+    }
 }
