c5135ab44086a13dce8075798e426c285bd0fa7f
gvnaakhilsurya <akhil.gonugunta@msitprogram.net>
2019-02-08 15:45:30 +0530

    "ads1_remedial_3_a1_1: 7/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- remedial/Remedial-3/Student.java
+++ remedial/Remedial-3/Student.java
@@ -68,35 +68,67 @@ class Student implements Comparable<Student>{
 		}
 
 	}
-	public int compareTo(Student that){
+	// public int compareTo(Student that){
+	// 	Student a = this;
+	// 	// if(a.rno>=that.rno) return -1;
+	// 	// if (a.sname.compareTo(that.sname) > 0)  return -1;
+	// 	// if (a.sname.compareTo(that.sname) < 0) return 1;
+	// 	// if(a.rno<=that.rno) return 1;
+	// 	// if(a.marks > that.marks) return -1;
+	// 	// if(a.marks < that.marks) return 1;
+	// 	// return 0;
+	// 	// if(a.rno>that.rno){
+	// 	// 	return -1;
+	// 	// }
+	// 	if(a.rno>that.rno) return -1;
+	// 	if(a.rno<that.rno) return 1;
+	// 	if (a.sname.compareTo(that.sname) == 0) {
+	// 		if(a.marks >= that.marks){
+	// 			return 1;
+	// 		}else{
+	// 			return -1;
+	// 		}
+	// 	}
+	// 	if (a.marks == that.marks) {
+	// 		if(a.rno>that.rno) return -1;
+	// 		if(a.rno<that.rno) return 1;
+
+			
+	// 	}
+		
+		
+	// 	return 0;
+		
+		
+		
+		
+
+	// }
+	public int compareTo(Student that) {
 		Student a = this;
-		// if(a.rno>=that.rno) return -1;
-		// if (a.sname.compareTo(that.sname) > 0)  return -1;
-		// if (a.sname.compareTo(that.sname) < 0) return 1;
-		// if(a.rno<=that.rno) return 1;
-		// if(a.marks > that.marks) return -1;
-		// if(a.marks < that.marks) return 1;
-		// return 0;
-		if(a.rno>=that.rno){
+		if (a.marks > that.marks) {
+			return -1;
+		}
+		if (a.marks < that.marks) {
 			return 1;
 		}
-
-		if(a.marks >= that.marks){
-			if (a.sname.compareTo(that.sname)<0) {
-				return 1;
-			}else{
+		if (a.marks == that.marks) {
+			if (a.sname.compareTo(that.sname) > 0) {
 				return -1;
 			}
+			if (a.sname.compareTo(that.sname) < 0) {
+				return 1;
+			}
+	    }
+		if (a.marks == that.marks && a.sname.equals(that.sname)) {
+			if (a.rno > that.rno) {
+				return -1;
+			}
+			if (a.rno < that.rno) {
+				return 1;
+			}
 		}
-		if(a.rno<=that.rno){
-			return -1;
-		} 
 		return 0;
-		
-		
-		
-		
-
 	}
 	public String toString(){
 		return this.rno+","+this.sname+","+ this.marks;
